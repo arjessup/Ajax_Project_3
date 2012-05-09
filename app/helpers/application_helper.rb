3deb04d9f30817f4c651ed0156fb88c02fdea4d7
Andrea Jessup <jessup.andrea@gmail.com>
2012-05-09 18:39:46 -0500

    Done with title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # Return on a per-page basis
+  def title
+    base_title = "AJAX Project 3"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
