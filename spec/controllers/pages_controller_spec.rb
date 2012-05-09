eb5c5730dfb3d21368270559920265cd7fce60bb
Andrea Jessup <jessup.andrea@gmail.com>
2012-05-09 18:32:29 -0500

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,46 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+  
+  before(:each) do
+    @base_title = "AJAX Project 3"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-  end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => @base_title + " | Home")
+    end
+  end # GET home
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
-  end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => @base_title + " | Contact")
+    end
+  end # GET contact
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => @base_title + " | About")
+    end
+  end # GET about
 
 end
